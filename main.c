3c6bddf9924d12bca712272d2714f772006ad066
Maxur1 <marcelo.soto01@alumnos.ucn.cl>
2021-12-10 09:45:29 -0300

    for establecido
    como se requieren todos los numeros hasta el 300, se agrega un for para que se evaluen todos los fibonacci uno tras otro en lugar de solo el numero introducido

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int fib(int n) {
     if (n < 2)
         return n;
     else {
-#pragma omp taks shared(i)
+#pragma omp task shared(i)
         i = fib(n - 1);
 #pragma omp task shared(j)
         j = fib(n - 2);
@@ -23,15 +23,25 @@ int main(int argc, char **argv)
 #pragma omp parallel
     {
 #pragma omp single
-        result = fib(n);
+       for (int i=1; i<n+1; i++) {
+          result = fib(i);
+        printf("fibonacci of %d",i);
+        printf(" is: %d\n", result);
+        printf("factorization is: ");
+        primerfactors(result, 2);
+        printf("\n\n");
+       }
+
     }
-    printf("result is %d\n", result);
-    primerfactors(result);
+
 
 
 }
 
 void primerfactors(int num){
+    if(num < 0){
+        return;
+    }
     int count;
     int primero = num;
     printf("the factorization is: ");
@@ -44,4 +54,38 @@ void primerfactors(int num){
             num = num/count;
         }
     }
+}
+void primerfactors2(int num, int count){
+
+    if(num <= 1){
+        return;
+    }
+    if(num % count == 0){
+        printf(" %d ",count);
+        num = num/count;
+
+    }else{
+        count++;
+    }
+#pragma omp task shared(k)
+    return primerfactors2(num,count);
+}
+
+int primerfactors3(int num, int count){
+    int k;
+
+    if(num <= 1){
+        return 1;
+    }
+    if(num % count == 0){
+        printf(" %d ",count);
+        num = num/count;
+
+    }else{
+        count++;
+    }
+#pragma omp task shared(k)
+    k = primerfactors3(num,count);
+    #pragma omp taskwait
+    return k;
 }
